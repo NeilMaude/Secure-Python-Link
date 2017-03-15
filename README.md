9ac62e358b91333be44f66eaeed4b76337ff8ebe
Neil Maude <neil.maude@gmail.com>
2017-03-15 17:00:44 +0000

    Added README and code
    feat: Added initial code
    
    First pass of code and README file

---
+++ README.md
@@ -0,0 +1,58 @@
+# Secure-Python-Link
+
+This is a set of tools to allow a calling process to pass credentials 
+to a Python script securely.  
+
+Requires Python 3.5.
+
+## Purpose
+The motivation for this code is to allow calling of Python scripts 
+from within the Arena Group **mstore** EDMS (electronic document 
+management system). The EDMS calls the Python script in order to 
+perform some action on EDMS data.  To do this, the Python program will
+need to log in to the EDMS database (usually Microsoft SQL-Server).
+It is not appropriate to pass credentials in plain, nor to store a
+username/password in clear within the Python scripts.
+
+Hence the protocol is as follows:
+
+*Create the username/password pairing
+*Run the `secure_account()` function to store the user/password
+*Record the keys returned
+*When calling Python programs pass the keys
+*The Python progam recovers the username/password using those keys
+
+This allows the username and password to be held in a persistent file
+(a pickle file) and for anyone configuring the calling process to not
+have direct database access (just these API-style keys).
+
+## Security
+It is assumed that a bad actor may have access to the Python scripts 
+and even the persistent store of credentials.  These are protected by 
+one-time-pad (OTP) encryption (i.e. _perfect_ cryptography).
+
+## Potential attacks
+The use of randomly generated OTPs is theoretically _perfect_ security.
+However, this does rely on the effectiveness of the random number 
+generator. 
+
+The internals of the process do use the last 2 digits of the encrypted
+store of the username and password to store the lengthe of the string
+which is used. An attacker with source access would know this, but the 
+use of an OTP means that every option is equally likely (i.e. no 
+useful information can be gained from the the last 2 digits). This did
+cause me a few concerns in development and I had to spend a little time
+staring at the code to satisfy myself that this (really useful) record
+of the length of each string was not making the system crackable.
+
+Note: one way I can think of to break this encryyption would be a brute
+force attack to find strings of alpha characters which look like a user
+name - it's fairly likely that user names will not have non-alpha 
+characters. Use of OTP makes every user name equally likely, so there 
+is no way to tell which is correct.
+
+A trivial weakness in this process would be if someone used the same
+string for the user name and password - in which case the OTPs are
+recoverable (this is the same as if the same encryption string was
+used to encrypt two plain-text strings - the encryption string is then
+directly recoverable). So don't do that...  
---
+++ WorkflowSecurity.py
@@ -0,0 +1,152 @@
+# Library module for adding Python modules to mstore EDMS with a level of security
+# Requirement is to use Python code on the server, without exposing user names/passwords
+# Principle is for the calling process (in mstore workflow) to pass application keys
+# These application keys are one-time-pad (OTP) strings used to decode the user and password for database connection
+# The encrypted user and password are stored in a standard pickle file with the Python modules
+# This is safe because the user/password are encrypted and cannot be retrieved, even with access to the Python code
+# Note: *MUST* use a separate OTP for each value to be encrypted, or perfect security is broken!
+# Neil Maude, March 2017
+
+import pickle
+from random import randint
+
+VALID_CHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!$%&*+=?#@' # Not '£' - issues in UTF-8
+OTP_LENGTH = 25
+
+# Function to create a random string of OTP characters of a set length
+def random_chars(length=50):
+    r_chars = ''
+    for i in range(0,length):
+        random = randint(0, len(VALID_CHARS)-1)       # Note: does not include len(VALID_CHARS) in range - would error
+        r_chars += VALID_CHARS[random]
+    return r_chars
+
+# Function to XOR two strings
+# Returns a boolean for success/failure, plus the XOR string
+def string_XOR(string1, string2):
+    fSuccess = False
+    output_string = ''
+    if len(string1) == len(string2):
+        a1 = bytearray(string1, 'utf-8')
+        a2 = bytearray(string2, 'utf-8')
+        for i in range(len(a1)):
+            a1[i] ^= a2[i]
+        output_string = a1.decode('utf-8')
+        fSuccess = True
+    return fSuccess, output_string
+
+# Function to pickle a list
+# Assumes that the path is valid
+def save_values(pfile, val_list):
+    data_pickle = {}
+    data_pickle['list'] = val_list
+    pickle.dump(data_pickle, open(pfile, 'wb'))
+    return True
+
+# Function to retrieve a pickled list
+def load_values(pfile):
+    val_pickle = pickle.load(open(pfile, 'rb'))
+    val_list = val_pickle['list']
+    return val_list
+
+# Function to pad a string with random characters
+def pad_string(input_string, required_length):
+    if len(input_string) < required_length:
+        # string not already long enough
+        r_char = random_chars(required_length - len(input_string))
+        return input_string + r_char
+    else:
+        return input_string
+
+# Function to take a string, pad it out, add the string length markers and encrypt it using a supplied key
+# Assumes that the plain string is 99 characters or less (i.e. 2 digits)
+def encrypt_string(plain_string, required_length, key_string):
+    len_plain = len(plain_string)
+    len_marker = str(len_plain).rjust(2,'0')
+    extended = pad_string(plain_string, required_length-2) + len_marker
+    #print(extended)
+    success, encrypted = string_XOR(extended, key_string)
+    return success, encrypted
+
+# Function to retrieve a string using a supplied key - with management of the original string length
+def retrieve_string(cipher_string, key_string):
+    success, decrypted = string_XOR(cipher_string, key_string)
+    if success:
+        # get the length from the end of the string
+        len_marker = decrypted[len(decrypted)-2:]
+        len_plain = int(len_marker)
+        decrypted = decrypted[0:len_plain]
+        return True, decrypted
+    else:
+        return False, 'Failed to decrypt'
+
+# Function to secure a user and password string to an acceptable level of security
+def secure_account(user, password, file):
+    # encrypt both values, ship out to a file
+    user_key = random_chars(OTP_LENGTH)
+    pass_key = random_chars(OTP_LENGTH)
+    u_success, e_user = encrypt_string(user, OTP_LENGTH, user_key)
+    p_success, e_pass = encrypt_string(password, OTP_LENGTH, pass_key)
+    if u_success and p_success:
+        values_list = [e_user, e_pass]
+        if save_values(file, values_list):
+            return True, user_key, pass_key
+        else:
+            return False, '', ''
+    else:
+        return False, '', ''
+
+def get_account_details(user_key, pass_key, file):
+    values_list = load_values(file)
+    if len(values_list) == 2:
+        u_success, user = retrieve_string(values_list[0], user_key)
+        p_success, password = retrieve_string(values_list[1], pass_key)
+        if u_success and p_success:
+            return True, user, password
+        else:
+            return False, '', ''
+    else:
+        return False, '', ''
+
+# Unit testing
+user = 'Neil'
+password = 'SomethingElse'
+file = 'Account1.p'
+success, user_key, pass_key = secure_account(user, password, file)
+print('Keys: ', user_key, pass_key)
+success, name, password2 = get_account_details(user_key, pass_key, file)
+print('Retrieved: ', name, password2)
+
+
+
+# print('Some random characters: ', random_chars(25))
+# secret = 'Hello!'
+# print('Secret   : ', secret)
+# key = random_chars(len(secret))
+# print('Key      : ', key)
+# success, encrypted = string_XOR(secret, key)
+# print('Encrypted: ', encrypted)
+# success, decrypted = string_XOR(encrypted, key)
+# print('Decrypted: ', decrypted)
+#
+# a, b = 'Left', 'Right'
+# val_list = [a] + [b]
+# sFile = 'test.p'
+# print(save_values(sFile, val_list))
+# in_list = load_values(sFile)
+# print('Retrieved values: ', in_list[0], in_list[1])
+#
+# print()
+# padded = pad_string('Hello',10)
+# print('Pad string (Hello,10):', padded, len(padded))
+
+# print()
+# print('Encryption test')
+# random_key = random_chars(100)
+# message = 'Hello World!'
+# success, encrypted = encrypt_string(message, 100, random_key)
+# print('Response: ', success, encrypted)
+#
+# print()
+# success, decrypted = retrieve_string(encrypted, random_key)
+# print('Response to retrieval: ', success, decrypted)
